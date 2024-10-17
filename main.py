1a583dcd429e139bf4f7a55bfe4a2702c92c299a
Armaan Mulani <armaanmulani08@gmail.com>
2024-10-17 22:39:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import search
 import speech_recognition as sr
 import os
 import webbrowser
@@ -13,9 +14,12 @@ def say(text):
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)
         r.pause_threshold = 1
         print("Listening...")
         audio = r.listen(source)
+        
+        r.energy_threshold = 400
 
     try:
         print("Recognizing...")
@@ -26,7 +30,7 @@ def takeCommand():
         print(f"Error: {e}")  # Print actual error for debugging
         return "Some error occurred.. Sorry"
 
-def storeCommand():
+def storeCommand(query):
     l = []
     if query == True:
         l.append(query)
@@ -38,15 +42,46 @@ if __name__ == '__main__':
     print("Pycharm")
     say("Hello, I am SENO A.I.")
     
-    sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], 
-             ["google", "https://www.google.com"], ["aniwatch", "https://www.aniwatchtv.to"]]
+    sites = [
+        ["google", "https://www.google.com"],
+        ["youtube", "https://www.youtube.com"],
+        ["facebook", "https://www.facebook.com"],
+        ["wikipedia", "https://www.wikipedia.org"],
+        ["amazon", "https://www.amazon.com"],
+        ["twitter", "https://www.twitter.com"],
+        ["my insta", "https://www.instagram.com/armaanmulani08/"],
+        ["reddit", "https://www.reddit.com"],
+        ["linked in", "https://www.linkedin.com/in/armaan-mulani-421bb9307/"],
+        ["netflix", "https://www.netflix.com"],
+        ["yahoo", "https://www.yahoo.com"],
+        ["bing", "https://www.bing.com"],
+        ["whatsapp", "https://www.whatsapp.com"],
+        ["quora", "https://www.quora.com"],
+        ["twitch", "https://www.twitch.tv"]]
+
 
     while True:
         query = takeCommand()
         if "some error" in query.lower():
             continue  # Skip iteration if an error occurred
-
+        
+        elif "search" in query.lower():
+            query
+            se = query.lower().split()
+            se.remove("search")
+            schr = ""
+            for i in se:
+                schr = schr + i + "+"
+            webbrowser.open(f"https://www.google.com/search?q={schr}")
+            
+            
+        elif "quit" in query.lower():
+            break
+        
+            
         for site in sites:
             if f"open {site[0]}".lower() in query.lower():
                 say("Yes Boss, opening " + site[0])
                 webbrowser.open(site[1])
+                #storeCommand(query)
+                
